5b6bd275d04caf822c8266686c55aeb1c9b74f77
Jesse Dao <robloxian362@gmail.com>
2025-11-17 13:23:47 +0700

    Add project files.

---
+++ WindowsForm/Customer.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForm
+{
+    internal class Customer
+    {
+        static string[] CustomerNameList = new string[100];
+        static string[] CustomerTypeList = new string[100];
+        static int[] ElectricityLastMonthAmount = new int[100];
+        static int[] ElectricityThisMonthAmount = new int[100];
+        static int CustomerQuantity = 0;
+
+        const string CUSTOMER_TYPE_HOUSEHOLD = "Household";
+        const string CUSTOMER_TYPE_PUBLIC_SERVICE = "Public Service";
+        /*
+         * input customer information
+         */
+        static void inputCustomerInfo()
+        {
+            bool isWrongInput = true;
+
+            while (isWrongInput)
+            {
+                isWrongInput = false;
+                Console.WriteLine("--------------------");
+                Console.WriteLine("Input information for new customer: ");
+                Console.WriteLine("Input customer name: ");
+                CustomerNameList[CustomerQuantity] = Console.ReadLine();
+                Console.WriteLine("Input customer type: (1 - Household; 2 - Public Service)");
+                int typeChoice = 1;
+                switch (typeChoice)
+                {
+                    case 1:
+                        CustomerTypeList[CustomerQuantity] = CUSTOMER_TYPE_HOUSEHOLD;
+                        break;
+                    case 2:
+                        CustomerTypeList[CustomerQuantity] = CUSTOMER_TYPE_PUBLIC_SERVICE;
+                        break;
+                    default:
+                        isWrongInput = true;
+                        CustomerTypeList[CustomerQuantity] = "Please only choose the values described";
+                        break;
+                }
+                CustomerTypeList[CustomerQuantity] = Console.ReadLine();
+                Console.WriteLine("Input electricity usage of last month: ");
+                ElectricityLastMonthAmount[CustomerQuantity] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Input electricity usage of this month: ");
+                ElectricityThisMonthAmount[CustomerQuantity] = Convert.ToInt32(Console.ReadLine());
+                if (ElectricityThisMonthAmount[CustomerQuantity] < ElectricityLastMonthAmount[CustomerQuantity])
+                {
+                    isWrongInput = true;
+                    Console.WriteLine("Invalid electricity usage value, default usage = 0");
+                    ElectricityThisMonthAmount[CustomerQuantity] = ElectricityLastMonthAmount[CustomerQuantity];
+                }
+                else
+                {
+                    isWrongInput = false;
+                }
+            }
+        }
+        /*
+         * Calculate electricity bill by level and eValue paramter
+         */
+        static int calculateBill(int eValue)
+        {
+            const int PRICE_L1 = 1984,
+                      PRICE_L2 = 2050,
+                      PRICE_L3 = 2380,
+                      PRICE_L4 = 2998,
+                      PRICE_L5 = 3350,
+                      PRICE_L6 = 3460,
+                      MAX_VAL_L1 = 50,
+                      MAX_VAL_L2 = 100,
+                      MAX_VAL_L3 = 200,
+                      MAX_VAL_L4 = 300,
+                      MAX_VAL_L5 = 400;
+            int billAmount = 0;
+            if (eValue < 0)
+            {
+                Console.WriteLine("Invalid Electricity Usage Value");
+            }
+            else if (eValue <= MAX_VAL_L1) // level 1
+            {
+                billAmount = eValue * PRICE_L1;
+            }
+            else if (eValue <= MAX_VAL_L2) // level 2
+            {
+                billAmount = (eValue - MAX_VAL_L1) * PRICE_L2
+                            + (MAX_VAL_L1 - 0) * PRICE_L1;
+            }
+            else if (eValue <= MAX_VAL_L3) // level 3
+            {
+                billAmount = (eValue - MAX_VAL_L2) * PRICE_L3
+                            + (MAX_VAL_L1 - 0) * PRICE_L1 + (MAX_VAL_L2 - MAX_VAL_L1) * PRICE_L2;
+            }
+            else if (eValue <= MAX_VAL_L4) // level 4
+            {
+                billAmount = (eValue - MAX_VAL_L3) * PRICE_L4
+                            + (MAX_VAL_L1 - 0) * PRICE_L1 + (MAX_VAL_L2 - MAX_VAL_L1) * PRICE_L2 + (MAX_VAL_L3 - MAX_VAL_L2) * PRICE_L3;
+            }
+            else if (eValue <= MAX_VAL_L5) // level 5
+            {
+                billAmount = (eValue - MAX_VAL_L4) * PRICE_L5
+                            + (MAX_VAL_L1 - 0) * PRICE_L1 + (MAX_VAL_L2 - MAX_VAL_L1) * PRICE_L2 + (MAX_VAL_L3 - MAX_VAL_L2) * PRICE_L3 + (MAX_VAL_L4 - MAX_VAL_L3) * PRICE_L4;
+            }
+            else // level 6
+            {
+                billAmount = (eValue - MAX_VAL_L5) * PRICE_L6
+                            + (MAX_VAL_L1 - 0) * PRICE_L1 + (MAX_VAL_L2 - MAX_VAL_L1) * PRICE_L2 + (MAX_VAL_L3 - MAX_VAL_L2) * PRICE_L3 + (MAX_VAL_L4 - MAX_VAL_L3) * PRICE_L4 + (MAX_VAL_L5 - MAX_VAL_L4) * PRICE_L5;
+            }
+            return billAmount;
+        }
+        static int calculateHouseHoldBill(int eValue)
+        {
+            return calculateBill(eValue);
+        }
+        static int calculatePublicServiceBill(int eValue)
+        {
+            return eValue * 2887;
+        }
+        /*
+         * print bill info
+         */
+        static void printBill(int index)
+        {
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine($"Dear Customer {CustomerNameList[index]}");
+            Console.WriteLine($"Customer Type: {CustomerTypeList[index]}");
+            int ElectricityUsage = ElectricityThisMonthAmount[index] - ElectricityLastMonthAmount[index];
+            int billAmount = 0;
+            switch (CustomerTypeList[index])
+            {
+                case CUSTOMER_TYPE_HOUSEHOLD:
+                    billAmount = calculateHouseHoldBill(ElectricityUsage);
+                    break;
+                case CUSTOMER_TYPE_PUBLIC_SERVICE:
+                    billAmount = calculatePublicServiceBill(ElectricityUsage);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Customer Type");
+                    break;
+            }
+            //int billAmount = calculateHouseHoldBill(ElectricityUsage);
+            Console.WriteLine($"Last month's electricity meter readings: {ElectricityLastMonthAmount[index]}");
+            Console.WriteLine($"This month's electricity meter readings: {ElectricityThisMonthAmount[index]}");
+            Console.WriteLine($"Electricity Usage This Month: {ElectricityUsage} kWh");
+            Console.WriteLine("Bill has been calculated");
+            Console.WriteLine($"Electricity Bill: {billAmount.ToString("N0")} VND"); // "N0" là Nờ và số 0 - anà
+            double billWithVAT = billAmount * 1.1;
+            Console.WriteLine($"Electricity Bill (Including 10% VAT): {billWithVAT.ToString("N0")} VND");
+            DateTime today = DateTime.Now;
+            DateTime fiveDaysLater = today.AddDays(5);
+            Console.WriteLine($"Payment Due Date: {fiveDaysLater.ToString("dd/MM/yyyy")}");
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.Write("Input Electricity Usage: ");
+            //int eValue = Convert.ToInt32(Console.ReadLine());
+            //int billAmount = calculateBill(eValue);
+            //printBill(billAmount);
+            int menuChoice = 1;
+            while (menuChoice != 0)
+            {
+
+                Console.WriteLine("---------Menu-----------");
+                Console.WriteLine("Please choose your action: ");
+                Console.WriteLine("1 - Add a new customer");
+                Console.WriteLine("2 - Print all customer bill");
+                Console.WriteLine("0 - Exit");
+                menuChoice = Convert.ToInt32(Console.ReadLine());
+                switch (menuChoice)
+                {
+                    case 1:
+                        inputCustomerInfo();
+                        CustomerQuantity++;
+                        break;
+                    case 2:
+                        for (int i = 0; i < CustomerQuantity; i++)
+                        {
+                            printBill(i);
+                        }
+                        printBill(0);
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting the program...");
+                        break;
+                    default:
+                        Console.WriteLine("Please choose valid values");
+                        break;
+                }
+                inputCustomerInfo();
+                printBill(CustomerQuantity - 1);
+            }
+
+        }
+    }
+}
+
---
+++ WindowsForm/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnName = new Button();
+            btnLastMonth = new Button();
+            btnThisMonth = new Button();
+            btnUsage = new Button();
+            btnBill = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            tbElectricityApplication = new Label();
+            cboType = new ComboBox();
+            btnSubmit = new Button();
+            btnDelete = new Button();
+            btnEdit = new Button();
+            lvCustomer = new ListView();
+            CustomerName = new ColumnHeader();
+            CustomerType = new ColumnHeader();
+            LastMonthElectricity = new ColumnHeader();
+            ThisMonthElectricity = new ColumnHeader();
+            UsageAmount = new ColumnHeader();
+            ElectricityBill = new ColumnHeader();
+            SuspendLayout();
+            // 
+            // btnName
+            // 
+            btnName.Location = new Point(168, 68);
+            btnName.Name = "btnName";
+            btnName.Size = new Size(261, 29);
+            btnName.TabIndex = 0;
+            btnName.UseVisualStyleBackColor = true;
+            // 
+            // btnLastMonth
+            // 
+            btnLastMonth.Location = new Point(252, 188);
+            btnLastMonth.Name = "btnLastMonth";
+            btnLastMonth.Size = new Size(185, 29);
+            btnLastMonth.TabIndex = 1;
+            btnLastMonth.UseVisualStyleBackColor = true;
+            // 
+            // btnThisMonth
+            // 
+            btnThisMonth.Location = new Point(252, 250);
+            btnThisMonth.Name = "btnThisMonth";
+            btnThisMonth.Size = new Size(185, 29);
+            btnThisMonth.TabIndex = 3;
+            btnThisMonth.UseVisualStyleBackColor = true;
+            // 
+            // btnUsage
+            // 
+            btnUsage.Location = new Point(244, 304);
+            btnUsage.Name = "btnUsage";
+            btnUsage.Size = new Size(185, 29);
+            btnUsage.TabIndex = 4;
+            btnUsage.UseVisualStyleBackColor = true;
+            // 
+            // btnBill
+            // 
+            btnBill.Location = new Point(168, 360);
+            btnBill.Name = "btnBill";
+            btnBill.Size = new Size(261, 29);
+            btnBill.TabIndex = 5;
+            btnBill.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AccessibleRole = AccessibleRole.None;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(27, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Customer Name:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(27, 364);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Electricity Bill:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(27, 308);
+            label3.Name = "label3";
+            label3.Size = new Size(190, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Usage Electricity Amount:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(27, 254);
+            label4.Name = "label4";
+            label4.Size = new Size(228, 20);
+            label4.TabIndex = 9;
+            label4.Text = "This Month Electricity Number:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(27, 192);
+            label5.Name = "label5";
+            label5.Size = new Size(228, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Last Month Electricity Number:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(27, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(118, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Customer Type:";
+            // 
+            // tbElectricityApplication
+            // 
+            tbElectricityApplication.AutoSize = true;
+            tbElectricityApplication.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            tbElectricityApplication.Location = new Point(302, 9);
+            tbElectricityApplication.Name = "tbElectricityApplication";
+            tbElectricityApplication.Size = new Size(311, 38);
+            tbElectricityApplication.TabIndex = 12;
+            tbElectricityApplication.Text = "Electricity Application";
+            tbElectricityApplication.Click += label7_Click;
+            // 
+            // cboType
+            // 
+            cboType.FormattingEnabled = true;
+            cboType.Items.AddRange(new object[] { "HouseHold", "Public Services", "Production Units", "Business Services" });
+            cboType.Location = new Point(168, 127);
+            cboType.Name = "cboType";
+            cboType.Size = new Size(261, 28);
+            cboType.TabIndex = 13;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSubmit.Location = new Point(272, 409);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(275, 29);
+            btnSubmit.TabIndex = 14;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            btnSubmit.Click += button1_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDelete.Location = new Point(553, 409);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(280, 29);
+            btnDelete.TabIndex = 15;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnEdit.Location = new Point(839, 409);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(282, 29);
+            btnEdit.TabIndex = 16;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // lvCustomer
+            // 
+            lvCustomer.Columns.AddRange(new ColumnHeader[] { CustomerName, CustomerType, LastMonthElectricity, ThisMonthElectricity, UsageAmount, ElectricityBill });
+            lvCustomer.GridLines = true;
+            lvCustomer.LabelWrap = false;
+            lvCustomer.Location = new Point(450, 68);
+            lvCustomer.Name = "lvCustomer";
+            lvCustomer.Size = new Size(671, 330);
+            lvCustomer.TabIndex = 17;
+            lvCustomer.UseCompatibleStateImageBehavior = false;
+            lvCustomer.View = View.Details;
+            // 
+            // CustomerName
+            // 
+            CustomerName.Text = "Customer Name";
+            CustomerName.Width = 120;
+            // 
+            // CustomerType
+            // 
+            CustomerType.Text = "Customy Type";
+            CustomerType.Width = 120;
+            // 
+            // LastMonthElectricity
+            // 
+            LastMonthElectricity.Text = "Last Month Electricity";
+            LastMonthElectricity.Width = 100;
+            // 
+            // ThisMonthElectricity
+            // 
+            ThisMonthElectricity.Text = "This Month Electricity";
+            ThisMonthElectricity.Width = 100;
+            // 
+            // UsageAmount
+            // 
+            UsageAmount.Text = "Usage Amount";
+            UsageAmount.Width = 120;
+            // 
+            // ElectricityBill
+            // 
+            ElectricityBill.Text = "Electricity Bill";
+            ElectricityBill.Width = 120;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.HotPink;
+            ClientSize = new Size(1144, 450);
+            Controls.Add(lvCustomer);
+            Controls.Add(btnEdit);
+            Controls.Add(btnDelete);
+            Controls.Add(btnSubmit);
+            Controls.Add(cboType);
+            Controls.Add(tbElectricityApplication);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnBill);
+            Controls.Add(btnUsage);
+            Controls.Add(btnThisMonth);
+            Controls.Add(btnLastMonth);
+            Controls.Add(btnName);
+            Name = "Form1";
+            Text = "Electricity Application";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnName;
+        private Button btnLastMonth;
+        private Button btnThisMonth;
+        private Button btnUsage;
+        private Button btnBill;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label tbElectricityApplication;
+        private ComboBox cboType;
+        private Button btnSubmit;
+        private Button btnDelete;
+        private Button btnEdit;
+        private ListView lvCustomer;
+        private ColumnHeader CustomerName;
+        private ColumnHeader CustomerType;
+        private ColumnHeader LastMonthElectricity;
+        private ColumnHeader ThisMonthElectricity;
+        private ColumnHeader UsageAmount;
+        private ColumnHeader ElectricityBill;
+    }
+}
